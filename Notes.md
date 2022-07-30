a8004274bef20d998d958e9fadaab00f6788126e
Shadi Zoldjalali <shadizold@gmail.com>
2022-07-29 21:31:44 -0700

    updating doc

--- Notes.md
+++ Notes.md
@@ -1,25 +1,43 @@
 # TODO
 - Restructure code - Shadi [ ]
 - Add score - [ ]
-- Add game - [ ]
+- Add gameover screen - [ ]
 - Add obstructions - [ ]
--
 
+---------------------------------------------------
+# Workflow Format
+- Making a change? Here's how
+- Make a new branch monitoring main branch
+- ex: git checkout -b branch_name_here origin/main
+- Make your changes and commit
+- rebase from target branch
+- ex: git pull --rebase
+- push your changes on this current branch, deal with conflicts
+- merge main with what you have
+- ex:   git checkout main
+        git merge branch_name_here
+
+---------------------------------------------------
 # BUG report format
 - Bug name
 - Assignee:
-- Reporter: 
+- Reporter:
+- Severity: (low, medium, high)
 - Description:
 - Date:
 - Workflow: (scheduled, in-progress, complete)
 - Comments:
-
 ---------------------------------------------------
 # BUG
-- Bug name
+- Code Restructuring
 - Assignee:
-- Reporter: 
+    Shadi
+- Reporter:
+    Shadi and Roham
 - Description:
+    Need to restructure code
 - Date:
-- Workflow: (scheduled, in-progress, complete)
+    July 29, 2022
+- Workflow:
+    In Progress
 - Comments:
--- snake.py
+++ snake.py
@@ -8,20 +8,27 @@ class Snake:
     def __init__(self, game):
         self.game = game
         self.size = game.TILE_SIZE
+        # starting rectangle of head of snake
         self.rect = pygame.rect.Rect([0, 0, game.TILE_SIZE - 2, game.TILE_SIZE - 2])
+        # find range of where starting head can be
         self.range = self.size // 2, self.game.WINDOW_SIZE - self.size // 2, self.size
         self.rect.center = self.get_random_position()
+
         self.direction = vec2(0, 0)
         self.step_delay = 100  # milliseconds
         self.time = 0
         self.length = 1
+        # keep segments of snake in a list
         self.segments = []
+        # using this dictionary so later when we move, we can check which directions we are able to go in
         self.directions = {pygame.K_UP: 1, pygame.K_DOWN: 1, pygame.K_LEFT: 1, pygame.K_RIGHT: 1}
 
+    # take in user input to move the snake
     def control(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and self.directions[pygame.K_UP]:
                 self.direction = vec2(0, -self.size)
+                # if you are going up, then you can only go up, left, or right
                 self.directions = {pygame.K_UP: 1, pygame.K_DOWN: 0, pygame.K_LEFT: 1, pygame.K_RIGHT: 1}
 
             if event.key == pygame.K_DOWN and self.directions[pygame.K_DOWN]:
@@ -36,6 +43,7 @@ class Snake:
                 self.direction = vec2(self.size, 0)
                 self.directions = {pygame.K_UP: 1, pygame.K_DOWN: 1, pygame.K_LEFT: 0, pygame.K_RIGHT: 1}
 
+    # controls speed of game
     def delta_time(self):
         time_now = pygame.time.get_ticks()
         if time_now - self.time > self.step_delay:
@@ -43,36 +51,43 @@ class Snake:
             return True
         return False
 
+    # gets random position for the snake and apple
     def get_random_position(self):
         return [randrange(*self.range), randrange(*self.range)]
 
+    # check if snake hits border of wall
     def check_borders(self):
         if self.rect.left < 0 or self.rect.right > self.game.WINDOW_SIZE:
             self.game.new_game()
         if self.rect.top < 0 or self.rect.bottom > self.game.WINDOW_SIZE:
             self.game.new_game()
 
+    # check if snake ate the food
     def check_food(self):
         if self.rect.center == self.game.food.rect.center:
             self.game.food.rect.center = self.get_random_position()
             self.length += 1
 
+    # check if the snake ate itself
     def check_selfeating(self):
         if len(self.segments) != len(set(segment.center for segment in self.segments)):
             self.game.new_game()
 
+    # mvove the snake
     def move(self):
         if self.delta_time():
             self.rect.move_ip(self.direction)
             self.segments.append(self.rect.copy())
             self.segments = self.segments[-self.length:]
 
+    # update screen
     def update(self):
         self.check_selfeating()
         self.check_borders()
         self.check_food()
         self.move()
 
+    # draw the snake
     def draw(self):
         [pygame.draw.rect(self.game.win, 'green', segment) for segment in self.segments]
 
